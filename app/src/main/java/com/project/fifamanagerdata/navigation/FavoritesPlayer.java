56e9f17bf4ec609f6c0aee8e5d02c478cbf8c872
junho <junho5508@naver.com>
2025-02-01 22:39:26 +0900

    즐겨찾기 기능 리사이클러 뷰 설정

--- app/src/main/java/com/project/fifamanagerdata/navigation/FavoritesPlayer.java
+++ app/src/main/java/com/project/fifamanagerdata/navigation/FavoritesPlayer.java
@@ -1,6 +1,9 @@
 package com.project.fifamanagerdata.navigation;
 
+import static androidx.core.app.ActivityCompat.recreate;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +11,28 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.project.fifamanagerdata.R;
+import com.project.fifamanagerdata.favoritePlayerRecylerView.FavoritePlayerRecylerViewAdapter;
+import com.project.fifamanagerdata.favoritePlayerRecylerView.FavoritePlayerRecylerViewData;
+import com.project.fifamanagerdata.favoritesPlayerRoomDB.PlayerDB;
+import com.project.fifamanagerdata.favoritesPlayerRoomDB.PlayerDataModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class FavoritesPlayer extends Fragment {
     private View view;
+    private RecyclerView recyclerView;
+    private FavoritePlayerRecylerViewAdapter adapter;
+    private LinearLayoutManager linearLayoutManager;
+    private ArrayList<FavoritePlayerRecylerViewData> recylerData;
+    private PlayerDB playerDB;
 
     @Nullable
     @Override
@@ -24,7 +44,44 @@ public class FavoritesPlayer extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // 뷰 초기화 후 필요한 작업 수행
-        // 예: view.findViewById를 통해 뷰의 요소에 접근하거나, 이벤트 리스너 설정 등
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.Rv_favorites);
+        recyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL));
+        linearLayoutManager = new LinearLayoutManager(view.getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        recylerData = new ArrayList<>();
+        adapter = new FavoritePlayerRecylerViewAdapter(recylerData);
+        recyclerView.setAdapter(adapter);
+
+        playerDB = PlayerDB.getInstance(view.getContext());
+        Executor executor = Executors.newSingleThreadExecutor();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                // Room DB에 접근하는 코드
+                List<PlayerDataModel> playerDataModelsList = playerDB.playerDao().getAllDataSortedByNickname();
+
+                // UI 업데이트는 메인 스레드에서
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (PlayerDataModel data : playerDataModelsList) {
+                            FavoritePlayerRecylerViewData favoritePlayer = new FavoritePlayerRecylerViewData();
+                            favoritePlayer.setFBplayer(data.getNickname());
+                            favoritePlayer.setFBOuid(data.getOuid()); // Ouid 필드가 있다면, 그 값을 설정
+
+                            recylerData.add(favoritePlayer); // 변환된 데이터를 리스트에 추가
+                        }
+
+                        // 어댑터에 데이터 갱신
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        });
+
+
+
     }
 }
