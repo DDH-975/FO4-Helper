afea2c9d27cd045cd786a9815c029808713e8299
junho <junho5508@naver.com>
2025-02-01 22:34:54 +0900

    즐겨찾기기능 리사이클러 뷰 어뎁터

---
+++ app/src/main/java/com/project/fifamanagerdata/favoritePlayerRecylerView/FavoritePlayerRecylerViewData.java
@@ -0,0 +1,28 @@
+package com.project.fifamanagerdata.favoritePlayerRecylerView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.room.PrimaryKey;
+
+import java.util.ArrayList;
+
+public class FavoritePlayerRecylerViewData {
+    private String FBplayer;
+    private String FBOuid;
+
+
+    public String getFBplayer() {
+        return FBplayer;
+    }
+
+    public void setFBplayer(String FBplayer) {
+        this.FBplayer = FBplayer;
+    }
+
+    public String getFBOuid() {
+        return FBOuid;
+    }
+
+    public void setFBOuid(String FBOuid) {
+        this.FBOuid = FBOuid;
+    }
+}
